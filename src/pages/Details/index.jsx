48acfd5cb350c8589f4dd76755f5759aa9dd5e92
Rennan Macarini <rennangmacarini@gmail.com>
2023-03-31 11:30:11 -0300

    atualizando

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import { Button } from '../../components/Button'
+import { Container } from './styles'
+
+export function Details(){
+  return(
+  <Container>
+    <h1>Hello World!</h1>
+
+    <Button title="Entrar"/>
+    <Button title="Sair"/>
+  </Container>
+  )
+}
