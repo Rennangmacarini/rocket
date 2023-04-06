96b8c22164654a06b81d876e9149aaf76a4e234d
Rennan Macarini <rennangmacarini@gmail.com>
2023-04-06 14:19:55 -0300

    finalizado

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,11 @@
 import {Container, Profile, Logout } from "./styles"
 import { RiShutDownLine } from "react-icons/ri"
 
+
 export function Header(){
     return(
         <Container>
-            <Profile>
+            <Profile to="/profile">
                 <img src="https://github.com/rennangmacarini.png" alt="Foto do usuário" />
                 <div>
                     <span>Bem-Vindo</span>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,11 @@
 import { ButtonText } from '../../components/ButtonText';
 import { Header } from '../../components/Header';
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiSearch } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
+import { Input } from '../../components/Input';
+import { Note } from '../../components/Note';
+import { Section } from '../../components/Section';
+
 
 export function Home(){
     return(
@@ -19,14 +23,24 @@ export function Home(){
         </Menu>
 
         <Search>
-
+       <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
         </Search>
 
         <Content>
+            <Section title="Minhas notas">
+                <Note data={{ 
+                    title: "React", 
+                    tags: [
+                        {id: "1", name: "react"},
+                        {id: "2", name: "rocketseat"}  
+                    ]
+                }}
+                />
 
+            </Section>
         </Content>
 
-        <NewNote>
+        <NewNote to="/new">
             <FiPlus/>
             Criar nota
         </NewNote>
